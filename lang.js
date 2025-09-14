e9dccd33c64a1ee155d255a076e9ba221c334873
kirbIndustries <189890658+kirbIndustries@users.noreply.github.com>
2025-09-14 10:34:02 +0100

    Create lang.js

---
+++ lang.js
@@ -0,0 +1,29 @@
+const LANG_EN_AILUATOR = {
+    TITLE: "Ailuator",
+    ERROR: "Error"
+};
+
+const LANG_JP_AILUATOR = {
+    TITLE: "計算機",
+    ERROR: "エラー"
+};
+
+const LANG_DE_AILUATOR = {
+    TITLE: "Ailuator",
+    ERROR: "Fehler"
+};
+
+const LANG_ES_AILUATOR = {
+    TITLE: "Ailuator",
+    ERROR: "Error"
+};
+
+const LANG_KO_AILUATOR = {
+    TITLE: "계산기",
+    ERROR: "오류"
+};
+
+const LANG_ZH_AILUATOR = {
+    TITLE: "计算器",
+    ERROR: "错误"
+};
